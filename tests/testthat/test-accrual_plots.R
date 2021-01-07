d42c919a0a14f6ebdb31221d1842a1397de7283c
aghaynes <aghaynes@gmail.com>
2021-01-07 13:35:56 +0100

    more figure tests

--- tests/testthat/test-accrual_plots.R
+++ tests/testthat/test-accrual_plots.R
@@ -8,6 +8,7 @@ site <- sample(1:3, 50, replace = TRUE)
 # Sys.setlocale("LC_ALL","English")
 
 df <- accrual_create_df(x)
+df2 <- accrual_create_df(x, by = site)
 
 # vdiffr::manage_cases()
 
@@ -28,10 +29,31 @@ test_that("cumulative plots", {
 
   fn <- function() accrual_plot_cum(df, xlabn = 8)
   expect_doppelganger("cumulative, xlabn", fn)
+  # doesnt seem to work
 
   fn <- function() accrual_plot_cum(df, xlabsrt = 90)
   expect_doppelganger("cumulative, xlabsrt", fn)
 
+
+  fn <- function() accrual_plot_cum(df2)
+  expect_doppelganger("cumulative site", fn)
+
+  fn <- function() accrual_plot_cum(df2, start_date = as.Date("2020-10-31"))
+  expect_doppelganger("cumulative site, early start", fn)
+
+  expect_error(accrual_plot_cum(df, start_date = as.Date("2020-11-20")))
+
+  fn <- function() accrual_plot_cum(df2, current_date = as.Date("2020-12-31"))
+  expect_doppelganger("cumulative site, end date", fn)
+
+  expect_error(accrual_plot_cum(df, current_date = as.Date("2020-12-20")))
+
+  fn <- function() accrual_plot_cum(df2, xlabn = 8)
+  expect_doppelganger("cumulative site, xlabn", fn)
+  # doesnt seem to work
+
+  fn <- function() accrual_plot_cum(df2, xlabsrt = 90)
+  expect_doppelganger("cumulative site, xlabsrt", fn)
 
 })
 
