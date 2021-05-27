94884c6f66ef8890917f66551b3b991f77e42ee0
aghaynes <aghaynes@gmail.com>
2021-05-27 16:25:22 +0200

    add engine option to plot and head option to print

--- R/methods.R
+++ R/methods.R
@@ -3,11 +3,12 @@
 #' @param x accrual_df object
 #' @param which one of \code{"cumulative"}, \code{"absolute"} or \code{"predict"}.
 #' Abbreviations are allowed.
+#' @param engine string to indicate the plotting engine (base/graphics or ggplot2)
 #' @param ... options passed to other functions
 #'
 #' @return a plot
 #' @export
-#'
+#' @seealso
 #' @examples
 #' set.seed(2020)
 #' enrollment_dates <- as.Date("2018-01-01") + sort(sample(1:30, 50, replace=TRUE))
@@ -16,24 +17,41 @@
 #' plot(accrual_df, "abs")
 #' plot(accrual_df, "pred", target = 50)
 
-plot.accrual_df <- function(x, which = "cum", ...){
+plot.accrual_df <- function(x
+                            , which = "cum"
+                            , engine = c("base", "ggplot2")
+                            , ...
+                            ){
 
   # which <- switch(as.character(which),
   #                 cumulative = "c",
   #                 )
 
   which <- match.arg(which, c("cumulative", "absolute", "predict"))
+  engine <- match.arg(engine)
 
-  if(which == "cumulative"){
-    accrual_plot_cum(x, ...)
+  if(engine == "base"){
+    if(which == "cumulative"){
+      accrual_plot_cum(x, ...)
+    }
+    if(which == "absolute"){
+      accrual_plot_abs(x, ...)
+    }
+    if(which == "predict"){
+      accrual_plot_predict(x, ...)
+    }
   }
-  if(which == "absolute"){
-    accrual_plot_abs(x, ...)
+  if(engine == "ggplot2"){
+    if(which == "cumulative"){
+      gg_accrual_plot_cum(x, ...)
+    }
+    if(which == "absolute"){
+      gg_accrual_plot_abs(x, ...)
+    }
+    if(which == "predict"){
+      gg_accrual_plot_predict(x, ...)
+    }
   }
-  if(which == "predict"){
-    accrual_plot_predict(x, ...)
-  }
-
 
 }
 
@@ -72,19 +90,26 @@ summary.accrual_df <- function(object, ...){
 #' @export
 #'
 #' @examples
-print.accrual_df <- function(x){
+#' set.seed(2020)
+#' enrollment_dates <- as.Date("2018-01-01") + sort(sample(1:30, 50, replace=TRUE))
+#' accrual_df<-accrual_create_df(enrollment_dates)
+#' print(accrual_df)
+#' print(accrual_df, head = TRUE)
+print.accrual_df <- function(x, head = FALSE, ...){
   mindate <- min(x$Date)
   maxdate <- max(x$Date)
   maxc <- max(x$Cumulative)
 
-  cat(paste(maxc, "participants recruited between", mindate, "and", maxdate))
+  cat(paste(maxc, "participants recruited between", mindate, "and", maxdate, "\n"))
+
+  if(head) print(head(x, ...), ...)
 
 }
 
 #' @rdname print.accrual_df
 #' @name print.accrual_list
 #' @export
-print.accrual_list <- function(x){
+print.accrual_list <- function(x, head = FALSE, ...){
   w <- which(names(x) == attr(x, "name_overall"))
   if(length(w) > 0) y <- x[-w]
   ncenter <- length(y)
@@ -92,6 +117,9 @@ print.accrual_list <- function(x){
   maxdate <- max(do.call("c", lapply(y, function(x) max(x$Date))))
   mindate <- min(do.call("c", lapply(y, function(x) min(x$Date))))
 
-  cat(paste(npart, "participants recruited between", mindate, "and", maxdate, "from", ncenter, "centers"))
+  cat(paste(npart, "participants recruited between", mindate, "and", maxdate, "from", ncenter, "centers\n"))
+
+  if(head) lapply(x, function(y) print.data.frame(head(y, ...), ...))
+
 }
 
