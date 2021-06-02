6c9972e92af768f1dd0b66b5842cd4910fe61508
aghaynes <aghaynes@gmail.com>
2021-06-02 09:54:30 +0200

    tolerance not needed

--- tests/testthat/test-accrual_predict.R
+++ tests/testthat/test-accrual_predict.R
@@ -17,12 +17,12 @@ test_that("accrual_predict result", {
 	x<-accrual_create_df(enrollment_dates,start_date=as.Date("2021-12-30"))
 	y<-accrual_linear_model(x,fill_up=FALSE)
 	z<-accrual_predict(x,y,target=365)
-	expect_true(z>as.Date("2022-12-31"), tolerance = .1)
+	expect_true(z>as.Date("2022-12-31"))
 
 	x<-accrual_create_df(enrollment_dates,current_date=as.Date("2022-02-01"))
 	y<-accrual_linear_model(x,fill_up=FALSE)
 	z<-accrual_predict(x,y,target=365)
-	expect_true(z>as.Date("2022-12-31"), tolerance = .1)
+	expect_true(z>as.Date("2022-12-31"))
 
 })
 
